73169a76aaaba81f2331de5223f476605324e534
anthonyrodarte <anthonyrodarte1422@gmail.com>
2021-07-01 18:26:29 -0700

    Added github and linkedin links.

--- src/components/Anthony.js
+++ src/components/Anthony.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import Selfie from '../images/anthony.jpeg';
+import Github from '../images/GitHub-logo.png';
+import LinkedIn from '../images/linkedin-logo-copy.png';
 
 const About = () => {
 	return (
@@ -11,8 +13,21 @@ const About = () => {
 			</div>
 			<img src={Selfie} className='selfie' alt='Anthony Rodarte' />
 			<div>
-				<h2>About</h2>
-				<p>Welcome to my website!</p>
+				<h2>Links</h2>
+				<a
+					href='https://github.com/anthonyrodarte'
+					target='_blank'
+					rel='noreferrer'
+				>
+					<img src={Github} className='github-logo' alt='Github' />
+				</a>
+				<a
+					href='https://www.linkedin.com/in/anthony-rodarte-99abb1155/'
+					target='_blank'
+					rel='noreferrer'
+				>
+					<img src={LinkedIn} className='linkedIn-logo' alt='LinkedIn' />
+				</a>
 			</div>
 		</div>
 	);
